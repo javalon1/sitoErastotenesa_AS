ce17d9cd3298f7d93f3c30cc92a27fb785ced0e0
Anna Smolinska <anias05@tlen.pl>
2019-05-18 12:53:11 +0100

    wersja uproszczona

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -2,11 +2,24 @@ package com.sda.algorytmy;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args ) {
-        
+public class App {
+    public static void main(String[] args) {
+        int k = 2;
+        while (k < 200) {
+            int licznik = 0;
+            for (int i = 2; i < k; i++) {
+                if (k % i == 0) {
+                    // System.out.println("jest dzielnik: "+i);
+                    licznik++;
+                }
             }
+            if (licznik == 0) {
+                System.out.println(k + " jest liczba pierwsza");
+            } else {
+                // System.out.println(k+ " nie jest liczba pierwsza");
+            }
+            k++;
+        }
+    }
 }
